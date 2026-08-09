35a4692df14e04f09c628c3ccec58a5b0843bdb2
BlueSky23895 <afzaalsubhani1234@gmail.com>
2026-08-09 18:11:30 +0500

    day 6

--- react-mastery/src/App.jsx
+++ react-mastery/src/App.jsx
@@ -1,21 +1,92 @@
 
-import { useState } from "react"
+import { useState } from "react";
 
 function App() {
-  const [Quantity, setQuantity] = useState(1)
-  const price = 1000
-  const total = Quantity * price
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const [course, setCourse] = useState("");
+
+  const [student, setStudent] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setStudent({
+      name: name,
+      email: email,
+      age: age,
+      course: course,
+    });
+  };
 
   return (
     <>
-      <h1>Price:{price}</h1>
-      <h1>Total:{total}</h1>
-      <h1>Quantity:{Quantity}</h1>
+      <h1>Student Registration</h1>
 
-      <button onClick={() => setQuantity(Quantity + 1)}>Increase</button>
-      <button onClick={() => setQuantity(Quantity > 0 ? Quantity - 1 : 0)}>Increase</button>
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <label>Age:</label>
+        <input
+          type="number"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <label>Course:</label>
+
+        <select
+          value={course}
+          onChange={(e) => setCourse(e.target.value)}
+        >
+          <option value="">Select Course</option>
+          <option value="React">React</option>
+          <option value="Python">Python</option>
+          <option value="Django">Django</option>
+          <option value="JavaScript">JavaScript</option>
+        </select>
+
+        <br />
+        <br />
+
+        <button type="submit">Submit</button>
+      </form>
+
+      {student && (
+        <>
+          <h1>Student Details</h1>
+
+          <p>Name: {student.name}</p>
+          <p>Email: {student.email}</p>
+          <p>Age: {student.age}</p>
+          <p>Course: {student.course}</p>
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
